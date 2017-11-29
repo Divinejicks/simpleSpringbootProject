ac22bfa136cd150f4d19ca4dafbba8d33c1d05fc
Divinejicks <divinejicks@gmail.com>
2017-11-29 18:27:28 +0100

    empty project

---
+++ schoolmanagement/src/main/java/com/divinejicks/schoolmanager/schoolmanagement/SchoolmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.divinejicks.schoolmanager.schoolmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolmanagementApplication.class, args);
+	}
+}
