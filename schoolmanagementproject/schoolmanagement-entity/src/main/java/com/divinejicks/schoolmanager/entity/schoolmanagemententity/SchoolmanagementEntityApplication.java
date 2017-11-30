6006c62c47cf5147305ef037e3bf14ec7a09eed1
Divinejicks <divinejicks@gmail.com>
2017-11-30 09:00:09 +0100

    Added a new project with the multi module

---
+++ schoolmanagementproject/schoolmanagement-entity/src/main/java/com/divinejicks/schoolmanager/entity/schoolmanagemententity/SchoolmanagementEntityApplication.java
@@ -0,0 +1,12 @@
+package com.divinejicks.schoolmanager.entity.schoolmanagemententity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmanagementEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolmanagementEntityApplication.class, args);
+	}
+}
---
+++ schoolmanagementproject/schoolmanagement-sms/src/main/java/com/divinejicks/schoolmanager/sms/schoolmanagementsms/SchoolmanagementSmsApplication.java
@@ -0,0 +1,12 @@
+package com.divinejicks.schoolmanager.sms.schoolmanagementsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmanagementSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolmanagementSmsApplication.class, args);
+	}
+}
