450b9395d67c1e696578c3452e74c92798bd4e77
Divinejicks <divinejicks@gmail.com>
2017-12-01 13:37:50 +0100

    Adjusted the multi module

--- schoolmanagementproject/schoolmanagement-entity/src/main/java/com/divinejicks/schoolmanager/entity/schoolmanagemententity/SchoolmanagementEntityApplication.java
+++ schoolmanagementproject/schoolmanagement-entity/src/main/java/com/divinejicks/schoolmanager/entity/schoolmanagemententity/SchoolmanagementEntityApplication.java
@@ -1,7 +1,9 @@
 package com.divinejicks.schoolmanager.entity.schoolmanagemententity;
 
+import com.divinejicks.schoolmanager.entity.schoolmanagemententity.dao.Faculty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SchoolmanagementEntityApplication {
@@ -9,4 +11,5 @@ public class SchoolmanagementEntityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolmanagementEntityApplication.class, args);
 	}
+
 }
--- schoolmanagementproject/schoolmanagement-sms/src/main/java/com/divinejicks/schoolmanager/sms/schoolmanagementsms/SchoolmanagementSmsApplication.java
+++ schoolmanagementproject/schoolmanagement-sms/src/main/java/com/divinejicks/schoolmanager/sms/schoolmanagementsms/SchoolmanagementSmsApplication.java
@@ -1,13 +1,20 @@
 package com.divinejicks.schoolmanager.sms.schoolmanagementsms;
 
+import com.divinejicks.schoolmanager.entity.schoolmanagemententity.dao.Faculty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-
+@RestController
 public class SchoolmanagementSmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolmanagementSmsApplication.class, args);
+	}
+	@GetMapping("")
+	Faculty getFaculty(){
+		return new Faculty();
 	}
 }
